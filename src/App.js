f6842074946a758a3de059d5320208620747bddc
cayde <caydebrummer@gmail.com>
2023-11-07 09:34:50 +0000

    they are showing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import SignUp from "./components/signup/Signup";
-import Feed from "./components/feed/Feed";
-import { useState } from "react";
+import Posts from "./components/post/Post";
 
-const getPosts = (setPosts) => {
-  fetch("/api/bulletinBoards")
-  .then((response) => response.json())
-  .then((data) => setPosts(data.posts))
-  .catch((error) => console.log(error));
-}
-
-function App() {
-
-  const [posts, setPosts] = useState([]);
-  getPosts(setPosts);
-
+const App = () => {
+  
   return (
     <div className="body">
-        <div className="App">
-          <SignUp />
-        </div>
-        <div className="Main">
-          <Feed post={posts} />
-        </div>
+      <div className="App">
+        <SignUp />
       </div>
+      <div className="Main">
+        <Posts/> 
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,15 +1,35 @@
-import React from "react";
+// src/components/Posts.js
 
-const Post = props => {
+import React, { useEffect, useState } from 'react';
 
-    return (
-        <div>
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
 
-            <div className="post-caption">
-                {props.post.caption}
-            </div>
-        </div>
-    );
-}
+  useEffect(() => {
+    fetch('https://localhost:3002/api/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data.posts);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
-export default Post;
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <img src={post.imgUrl} alt={post.caption} />
+            <h2>{post.caption}</h2>
+            <p>Likes: {post.likes}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
